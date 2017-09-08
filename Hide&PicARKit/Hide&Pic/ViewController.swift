ff7a9e7d46d39c50231e152b3f417143c6b8c714
Leonard Zou <zouleonard@gmail.com>
2017-09-09 09:32:07 +1000

    remove default code that was autogenerated at creation of project also update Pods warnings settings

--- Hide&PicARKit/Hide&Pic/ViewController.swift
+++ Hide&PicARKit/Hide&Pic/ViewController.swift
@@ -14,27 +14,13 @@ import CoreLocation
 import MapKit
 
 
-class ViewController: UIViewController/*, ARSCNViewDelegate*/ {
+class ViewController: UIViewController {
     // This is the main class we will use from ARCL pod
     var sceneLocationView = SceneLocationView()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-/*
-        
-        // Set the view's delegate
-        sceneView.delegate = self
-
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
-*/
         
         // Code from ARCL readme file under Quick Start Guide heading
         sceneLocationView.run()
@@ -97,20 +83,4 @@ class ViewController: UIViewController/*, ARSCNViewDelegate*/ {
         // Release any cached data, images, etc that aren't in use.
     }
 
-    // MARK: - ARSCNViewDelegate
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
 }
