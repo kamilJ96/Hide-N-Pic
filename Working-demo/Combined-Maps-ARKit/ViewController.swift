394a2e9284b23685a66336d08c43932a0cc3f1e1
Leonard Zou <zouleonard@gmail.com>
2017-10-04 13:00:09 +1100

    pin displays now work in combined maps w/ ARKit app. Under Working demo folder

--- Working-demo/Combined-Maps-ARKit/ViewController.swift
+++ Working-demo/Combined-Maps-ARKit/ViewController.swift
@@ -10,69 +10,24 @@ import UIKit
 import CoreLocation
 import MapKit
 import CoreGraphics
-import ARCL
 
 class ViewController: UIViewController, CLLocationManagerDelegate   {
-
-    @IBOutlet var sceneLocationView: SceneLocationView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // for the bottom map view
         checkLocation()
-        
-        sceneLocationView.run()
-        
-        // places around Alice Hoy and SMAC and ERC
-        // 1
-        addNewLocationAnnotationNodeToScene(latitude: -37.799288, longitude: 144.963672)
-        // 2
-        addNewLocationAnnotationNodeToScene(latitude: -37.799284, longitude: 144.963570)
-        // 3
-        addNewLocationAnnotationNodeToScene(latitude: -37.799396, longitude: 144.963460)
-        // 4
-        addNewLocationAnnotationNodeToScene(latitude: -37.799485, longitude: 144.963441)
-        // 5
-        addNewLocationAnnotationNodeToScene(latitude: -37.799549, longitude: 144.963350)
-        // 6
-        addNewLocationAnnotationNodeToScene(latitude: -37.799508, longitude: 144.963114)
-        // 7
-        addNewLocationAnnotationNodeToScene(latitude: -37.799286, longitude: 144.963130)
-        // 8
-        addNewLocationAnnotationNodeToScene(latitude: -37.799061, longitude: 144.963205)
-        // 9
-        addNewLocationAnnotationNodeToScene(latitude: -37.798828, longitude: 144.963208)
-        // 10
-        addNewLocationAnnotationNodeToScene(latitude: -37.798688, longitude: 144.963355)
-        // 11
-        addNewLocationAnnotationNodeToScene(latitude: -37.798633, longitude: 144.963540)
         
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-        sceneLocationView.pause()
-    }
-    
-    // MARK: - ARCL code, added by Leonard
-    func addNewLocationAnnotationNodeToScene(
-        latitude: CLLocationDegrees, longitude: CLLocationDegrees,
-        altitude: CLLocationDistance = 50,
-        scaleRelativeToDistance: Bool = true)
-    {
-        let image = UIImage(named: "pin")!
-        
-        let locationCoordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
-        let location = CLLocation(coordinate: locationCoordinate, altitude: altitude)
-        
-        let annotationNode = LocationAnnotationNode(location: location, image: image)
-        annotationNode.scaleRelativeToDistance = scaleRelativeToDistance
-        sceneLocationView.addLocationNodeWithConfirmedLocation(locationNode: annotationNode)
-        print("Coordinate added: \(latitude), \(longitude)") // DEBUG
     }
 
 
-    // MARK: - mapview controller code
+    // MARK: - mapview code
     
     @IBOutlet weak var mapView: MKMapView!
     let regionRadius: CLLocationDistance = 100
@@ -110,8 +65,8 @@ class ViewController: UIViewController, CLLocationManagerDelegate   {
     /* Send player and enemy data to the ExpandedViewController */
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let navVC = segue.destination as? UINavigationController
-        let expandedVC = navVC?.viewControllers.first as! ExpandedMapViewController
-        expandedVC.playerLocation = locationManager.location!
+        let expandedVC = navVC?.viewControllers.first as? ExpandedMapViewController
+        expandedVC?.playerLocation = locationManager.location!
     }
     
 }
