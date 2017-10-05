c67b6c8e063642acdc0dc3f2f113a4f6d6cbdffa
Leonard Zou <zouleonard@gmail.com>
2017-10-05 13:13:39 +1100

    add TimeLengths struct for collection of values

---
+++ Working-demo/Combined-Maps-ARKit/TimeLengths.swift
@@ -0,0 +1,13 @@
+//
+//  TimeLengths.swift
+//  Combined-Maps-ARKit
+//
+//  Created by Leonard Zou on 5/10/17.
+//  Copyright © 2017 VanillaThunder. All rights reserved.
+//
+
+import Foundation
+
+struct TimeLengths {
+    let pinDisplayTime = 15 // seconds. Length of time that opponent's location pins are displayed in AR View, before disappearing
+}
