411388114ac9ce115c1252ee08b09995d368a781
Karan Singh <karandeep96@hotmail.com>
2017-10-14 14:12:38 +1100

    (Hopefully) retrieves users from the database to the friends list

--- Hide&PicFromScratch/Hide&PicFromScratch/PreGame/FriendsListTableViewController.swift
+++ Hide&PicFromScratch/Hide&PicFromScratch/PreGame/FriendsListTableViewController.swift
@@ -16,17 +16,48 @@ class FriendsListTableViewController: UITableViewController {
     
     var current_user = Auth.auth().currentUser
     
+    let cellId = "cellId"
+    var users = [User]()
+    var receiver_name = String()
+    var receiver_id = String()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         print("\ncurrent_user = \(String(describing: current_user))\n") // DEBUG
         print() // DEBUG
+        
+        tableView.register(UserCell.self, forCellReuseIdentifier: cellId)
+        fetchUser()
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+    
+    // Retrieving the current users of the app (however its every single user atm)
+    func fetchUser() {
+        Database.database().reference().child("user_profile").observe(.childAdded, with: {
+            (Snapshot) in
+            
+            if let dictionary = Snapshot.value as? [String: AnyObject] {
+                let user = User()
+                // If you use this setter, app will crash if properties dont match with firebase
+                user.name = dictionary["name"] as! String
+                user.id = Snapshot.key
+                // If current user's name equals a database user
+                if self.current_user?.displayName == user.name {
+                    // Do nothing
+                } else {
+                    self.users.append(user)
+                }
+                DispatchQueue.main.async(execute: {
+                    self.tableView.reloadData()
+                });
+            }
+        })
     }
 
     override func didReceiveMemoryWarning() {
@@ -43,7 +74,43 @@ class FriendsListTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return users.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // let cell = UITableViewCell(style: .subtitle, reuseIdentifier: cellId)
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        let user = users[indexPath.row]
+        cell.textLabel?.text = user.name
+        return cell
+    }
+    
+    // Handles the selection of a user in the DB
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.receiver_name = self.users[indexPath.row].name!
+        self.receiver_id = self.users[indexPath.row].id!
+        self.performSegue(withIdentifier: "Chat", sender: self.users[indexPath.row])
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        let navVc = segue.destination as! UINavigationController
+        let chatVc = navVc.viewControllers.first as! ChatViewController
+        chatVc.senderId = current_user?.uid
+        chatVc.senderDisplayName = self.current_user?.displayName
+        chatVc.receiverId = self.receiver_id
+        chatVc.receiverName = self.receiver_name
+    }
+    
+    class UserCell: UITableViewCell {
+        override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+            super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+        }
+        
+        required init?(coder aDecoder: NSCoder) {
+            fatalError("has not been implemented correctly")
+        }
+        
     }
 
     /*
